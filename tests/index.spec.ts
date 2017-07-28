08260e6a0b5c7ffb455b16f1cdef60d50a071563
Vajahath Ahmed <vajuoff.1@gmail.com>
2017-07-28 15:38:58 +0530

    tests pending

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,10 +1,1 @@
-import {expect} from 'chai';
-import add from '../src/index';
-
-describe('Testing addition', ()=>{
-    it('returns 11 for 3,8', done=>{
-        let p = add(3,8);
-        expect(p).to.equal(11)
-        done();
-    })
-})
+describe('tests will be described later', ()=>{})
